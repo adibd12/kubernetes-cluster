f67f7a22539a4d2b43e2db745e0c12d9e937063a
adibd12 <58192161+adibd12@users.noreply.github.com>
2023-02-19 16:30:02 +0200

    Create resources.tf

---
+++ resources.tf
@@ -0,0 +1,54 @@
+
+###-keycloak
+resource "helm_release" "keycloak" {
+  name       = "keycloak"
+
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "keycloak"
+  create_namespace = true
+  timeout = 900
+  values = [
+    file("config/keycloak-value.yaml")
+  ]
+  depends_on = [helm_release.argocd]
+}
+
+###-kong
+resource "helm_release" "kong" {
+  name       = "kong"
+
+  repository = "https://charts.konghq.com"
+  chart      = "kong"
+  create_namespace = true
+  wait             = true
+  set {
+    name  = "ingressController.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "admin.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "admin.http.enabled"
+    value = "true"
+  }
+  
+  set {
+    name  = "proxy.enabled"
+    value = "true"
+  }
+  
+  set {
+    name  = "proxy.type"
+    value = "ClusterIP"
+  }
+
+  set {
+    name  = "ingressController.installCRDs"
+    value = "false"
+  }
+  depends_on = [helm_release.argocd]
+}
